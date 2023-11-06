d7e30aa0c26114a1ddfffab0eb98103a26c80ddd
paulg44 <paulgarton1984@hotmail.com>
2023-11-06 20:50:03 +0000

    change layout to homepage

--- ssj/src/Components/Homepage/Homepage.js
+++ ssj/src/Components/Homepage/Homepage.js
@@ -8,15 +8,15 @@ function Homepage() {
         <div className="homepageHeader">
           <p>Welcome to</p>
           <h1>Shardlow St James FC</h1>
-          <div className="about">
-            <h2>About Us</h2>
-            <p className="aboutDesc">
-              Shardlow St James F.C is a Sunday morning football team that play
-              in the Long Eaton Sunday League. We are one of the oldest amateur
-              football teams in the country and play our games at The Wharf,
-              Shardlow, Derbyshire.
-            </p>
-          </div>
+        </div>
+        <div className="about">
+          <h2>About Us</h2>
+          <p className="aboutDesc">
+            Shardlow St James F.C is a Sunday morning football team that play in
+            the Long Eaton Sunday League. We are one of the oldest amateur
+            football teams in the country and play our games at The Wharf,
+            Shardlow, Derbyshire.
+          </p>
         </div>
       </div>
     </div>
