2c54aab3e61f47dea610eb5486b3fe6f54a34a86
paulg44 <paulgarton1984@hotmail.com>
2023-11-08 19:13:25 +0000

    add more styling and background to events page

--- ssj/src/Components/Pages/Events/Events.js
+++ ssj/src/Components/Pages/Events/Events.js
@@ -6,28 +6,30 @@ import EventsContainer from "../../SmallComponents/EventsContainer/EventsContain
 function Events({ id }) {
   return (
     <div id={id} className="events">
+      <div className="homepageObject eventsObject"></div>
       <h3>Upcoming Events</h3>
+      <p className="eventsSubHeader">Fixtures</p>
       <div className="eventList">
         <EventsContainer
-          eventTitle="Charity Match"
+          eventTitle="First"
           eventDate="22/6/2023"
           eventDesc="Charity match in aid of insert here. More description, this is a placeholder for now."
           eventLocation="Shardlow, the Wharf"
         />
         <EventsContainer
-          eventTitle="Charity Match"
+          eventTitle="Reserves"
           eventDate="22/6/2023"
           eventDesc="Charity match in aid of insert here. More description, this is a placeholder for now."
           eventLocation="Shardlow, the Wharf"
         />
         <EventsContainer
-          eventTitle="Charity Match"
+          eventTitle="U16"
           eventDate="22/6/2023"
           eventDesc="Charity match in aid of insert here. More description, this is a placeholder for now."
           eventLocation="Shardlow, the Wharf"
         />
         <EventsContainer
-          eventTitle="Charity Match"
+          eventTitle="Vets"
           eventDate="22/6/2023"
           eventDesc="Charity match in aid of insert here. More description, this is a placeholder for now."
           eventLocation="Shardlow, the Wharf"
