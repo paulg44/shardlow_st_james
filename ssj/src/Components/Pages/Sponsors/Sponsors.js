4ec72b9c5982d43ac932750653906b05bcb4d4fd
paulg44 <paulgarton1984@hotmail.com>
2023-11-10 19:16:45 +0000

    add details to sponsors page

--- ssj/src/Components/Pages/Sponsors/Sponsors.js
+++ ssj/src/Components/Pages/Sponsors/Sponsors.js
@@ -1,13 +1,39 @@
 // Sponsors Component
 
 import "./Sponsors.css";
+import foremostImg from "../../../IMG/foremost-removebg-preview.png";
+import howdenImg from "../../../IMG/howdens_ssj.png";
+import electricImg from "../../../IMG/electric_center_ssj-removebg-preview.png";
+import clockImg from "../../../IMG/clock_ssj-removebg-preview.png";
 import SponsorsContainer from "./SponsorsContainer/SponsorsContainer";
 
 function Sponsors() {
   return (
     <div className="sponsors">
-      <h3>Sponsors</h3>
-      <SponsorsContainer />
+      <div className="homepageObject sponsorsObject"></div>
+      <h3>Proudly Sponsored By...</h3>
+      <div className="sponsorsList">
+        <SponsorsContainer
+          sponsorLinkURL="https://foremost-security.co.uk/"
+          sponsorIMG={foremostImg}
+          sponsorIMGAlt="foremost image"
+        />
+        <SponsorsContainer
+          sponsorLinkURL="https://www.howdens.com/"
+          sponsorIMG={howdenImg}
+          sponsorIMGAlt="howden image"
+        />
+        <SponsorsContainer
+          sponsorLinkURL="https://www.clockwarehousepub.co.uk/"
+          sponsorIMG={clockImg}
+          sponsorIMGAlt="clockwarehouse image"
+        />
+        <SponsorsContainer
+          sponsorLinkURL="https://www.electric-center.co.uk/"
+          sponsorIMG={electricImg}
+          sponsorIMGAlt="electric centre image"
+        />
+      </div>
     </div>
   );
 }
--- ssj/src/Components/Pages/Sponsors/SponsorsContainer/SponsorsContainer.js
+++ ssj/src/Components/Pages/Sponsors/SponsorsContainer/SponsorsContainer.js
@@ -5,9 +5,8 @@ import "./SponsorsContainer.css";
 function SponsorsContainer(props) {
   return (
     <div className="sponsor">
-      <img src={props.sponsorIMG} alt={props.sponsorIMGAlt}></img>
-      <a href={props.sponsorLink} target="_blank" rel="noreferrer">
-        Link
+      <a href={props.sponsorLinkURL} target="_blank" rel="noreferrer">
+        <img src={props.sponsorIMG} alt={props.sponsorIMGAlt}></img>
       </a>
     </div>
   );
