73c55eb26e71e6ced77c2a9135f3f91573c854fc
paulg44 <paulgarton1984@hotmail.com>
2023-11-17 17:52:06 +0000

    yet another attempt to make footer look good on mobile

--- ssj/src/Components/SmallComponents/Footer/Footer.js
+++ ssj/src/Components/SmallComponents/Footer/Footer.js
@@ -7,27 +7,25 @@ import { FaTwitter } from "react-icons/fa";
 function Footer() {
   return (
     <footer className="footer">
-      <div className="footerContainer">
-        <div className="footerLinks">
-          <a
-            href="https://www.facebook.com/groups/819820298102783"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaFacebook />
-          </a>
-          <a
-            href="https://twitter.com/FCShardlow"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FaTwitter />
-          </a>
-        </div>
-        <div className="footerInfo">
-          <p>SSJ &copy; 2023</p>
-          <p>Designed & Built by P.Garton</p>
-        </div>
+      <div className="footerLinks">
+        <a
+          href="https://www.facebook.com/groups/819820298102783"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaFacebook />
+        </a>
+        <a
+          href="https://twitter.com/FCShardlow"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaTwitter />
+        </a>
+      </div>
+      <div className="footerInfo">
+        <p>SSJ &copy; 2023</p>
+        <p>Designed & Built by P.Garton</p>
       </div>
     </footer>
   );
