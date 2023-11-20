6b4b648d7527268cf61a1fe24b7ddac78c0824ac
paulg44 <paulgarton1984@hotmail.com>
2023-11-20 20:40:26 +0000

    add shop item file folder, basic component and import to shop

--- ssj/src/Components/Pages/Shop/Shop.js
+++ ssj/src/Components/Pages/Shop/Shop.js
@@ -1,11 +1,13 @@
 // Shop Component
 
 import "./Shop.css";
+import ShopItem from "./ShopItem/ShopItem";
 
 function Shop() {
   return (
     <div className="shop">
       <h2>Shop!</h2>
+      <ShopItem />
     </div>
   );
 }
---
+++ ssj/src/Components/Pages/Shop/ShopItem/ShopItem.js
@@ -0,0 +1,13 @@
+// Shop Item Component
+
+import "./ShopItem.css";
+
+function ShopItem() {
+  return (
+    <div>
+      <p>Shop Item</p>
+    </div>
+  );
+}
+
+export default ShopItem;
