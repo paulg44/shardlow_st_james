342563535db0bd1a871b8c5c1bd35bf1b7a468af
paulg44 <paulgarton1984@hotmail.com>
2024-02-08 16:53:02 +0000

    set up running server

---
+++ server/server.js
@@ -0,0 +1,28 @@
+// Server file
+
+import express from "express";
+import pkg from "pg";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const { Pool } = pkg;
+
+const app = express();
+const port = process.env.REACT_APP_PORT;
+const connectionString = process.env.REACT_APP_SSJ_DB_STRING;
+
+export const pool = new Pool({
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
