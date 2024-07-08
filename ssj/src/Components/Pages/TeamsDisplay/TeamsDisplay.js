859e093b78a6efb3e1da6200dc31e02677523d79
Paul Garton <paulgarton1984@hotmail.com>
2024-07-08 20:55:02 +0100

    comment out unsed code that breaks deployment

--- ssj/src/Components/Pages/TeamsDisplay/TeamsDisplay.js
+++ ssj/src/Components/Pages/TeamsDisplay/TeamsDisplay.js
@@ -9,9 +9,9 @@
 */
 
 import { useEffect } from "react";
-import ResultsFixtures from "./Results/Fixtures/ResultsFixtures.js";
+// import ResultsFixtures from "./Results/Fixtures/ResultsFixtures.js";
 import "./TeamsDisplay.css";
-import Table from "./Table/Table.js";
+// import Table from "./Table/Table.js";
 
 function TeamsDisplay({ teamsData, activeTeam }) {
   useEffect(() => {
