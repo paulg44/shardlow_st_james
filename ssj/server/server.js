a9778e7a936137342c05e7d654190ea16a584896
Paul Garton <paulgarton1984@hotmail.com>
2024-10-22 19:59:09 +0100

    add server using node and express

---
+++ ssj/server/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import fs from "fs-extra";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+const PORT = process.env.REACT_APP_SERVER_PORT;
+
+app.get("/instagramData", async (req, res) => {
+  try {
+    const scrapedData = await fs.readFile("instagramData.json", "utf-8");
+    res.json(JSON.parse(scrapedData));
+  } catch (error) {
+    console.error("Error reading json data server side", error);
+    res
+      .status(500)
+      .json({ error: "Failed to load scraped data fromm Instagram" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
