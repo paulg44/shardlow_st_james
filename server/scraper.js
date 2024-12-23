2354d8d9f17510ca3fd87f0c0e7690e8d1fbf6f6
Paul Garton <paulgarton1984@hotmail.com>
2024-12-23 17:37:17 +0000

    create cron job direct in scraper

--- server/scraper.js
+++ server/scraper.js
@@ -1,36 +1,64 @@
 import playwright from "playwright";
 import fs from "fs-extra";
+import cron from "node-cron";
+import dotenv from "dotenv";
+import simpleGit from "simple-git";
+
+dotenv.config();
+
+const git = simpleGit({
+  baseDir: process.cwd(),
+  config: [
+    `user.name=${process.env.GITHUB_USERNAME}`,
+    `user.email=${process.env.GITHUB_EMAIL}`,
+  ],
+});
+
+const remoteURL = `https://${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_USERNAME}/shardlow_st_james.git`;
 
 export async function scrapeInstagramWebsite(url) {
   const browser = await playwright.chromium.launch({ headless: true });
 
   const context = await browser.newContext();
-
   const page = await context.newPage();
 
-  await page.goto(url);
-
-  await page.waitForTimeout(3000);
-
-  await page.getByRole("button", { name: /allow all cookies/i }).click();
-
-  await page.waitForTimeout(3000);
-
-  const linkAtrributeHrefs = await page.evaluate(() => {
-    const mainSection = document.querySelector("section");
-
-    if (mainSection) {
-      return Array.from(document.links).map((item) => item.href);
-    }
-    return [];
-  });
-
-  console.log(linkAtrributeHrefs);
-
-  fs.writeFile(
-    "instagramData.json",
-    JSON.stringify(linkAtrributeHrefs, null, 2)
-  );
-
-  await browser.close();
+  try {
+    await page.goto(url);
+    await page.waitForTimeout(3000);
+    await page.getByRole("button", { name: /allow all cookies/i }).click();
+    await page.waitForTimeout(3000);
+    const linkAtrributeHrefs = await page.evaluate(() => {
+      const mainSection = document.querySelector("section");
+      if (mainSection) {
+        return Array.from(document.links).map((item) => item.href);
+      }
+      return [];
+    });
+    console.log(linkAtrributeHrefs);
+    await fs.writeFile(
+      "instagramData.json",
+      JSON.stringify(linkAtrributeHrefs, null, 2)
+    );
+  } catch (error) {
+    console.error("Error during scraping:", error);
+  } finally {
+    await browser.close();
+  }
 }
+
+cron.schedule("40 17 * * *", async () => {
+  try {
+    console.log("Running scheduled scraper....");
+
+    await scrapeInstagramWebsite("https://www.instagram.com/shardlowstjamesfc");
+
+    await git.addRemote("origin", remoteURL).catch(() => {});
+    await git.add("./instagramData.json");
+    await git.commit("Daily update of SSJ Instagram data from scraper");
+    await git.push("origin", "main");
+
+    console.log("Data scraped and pushed successfully");
+  } catch (error) {
+    console.error("Error during scheduled cron job:", error);
+  }
+});
